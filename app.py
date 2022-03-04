4b90e5e9d222045f00ddd14d2537f283fd6682ae
aadityaagrawal <74168474+aadityaagrawal@users.noreply.github.com>
2022-03-04 16:55:18 +0530

    Video dependency added

--- app.py
+++ app.py
@@ -23,6 +23,59 @@ def main():
     #Function to read the image  
     #image = cv2.imread(image) 
     # Function to resize the image  
+    
+    if Video is not None:
+        cap = cv2.VideoCapture("Video_path")
+        while(cap.isOpened()):
+            ret,frame = cap.read()
+            if ret==True :
+                image = imutils.resize(frame,height=620,width=480)
+            #Convert BGR image to GRAYSCALE
+                gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                #Canny edge detection to detect edges in an image 
+                edged=cv2.Canny(gray_image,30,200)
+                # Function to join similar edges 
+                #Take two arguments : 
+                #First:: It take all the contours but doesn't create parent-child relationship
+                #Second:: It specify to take corner points off the counter
+                cnts,new = cv2.findContours(edged.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)   
+                image1=image.copy()
+                #This function is used to create counters based to given counter cordinates
+                cv2.drawContours(image1,cnts,-1,(0,255,0),3)
+                #Here we are sorting the conters and neglecting all the conters whose area is less than 20
+                cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:20]
+                screenCnt = None
+                image2 = image.copy()
+                for c in cnts:
+                    #To determine sqaure cure among the identified contours
+                    perimeter = cv2.arcLength(c,True)
+                    approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
+                    if len(approx) == 4: 
+                        screenCnt = approx
+                        x,y,w,h = cv2.boundingRect(c) 
+                        new_img=image[y:y+h,x+3:x+w+3]
+                        resized = cv2.resize(new_img,dsize=None,fx=4,fy=4)
+                        invert = cv2.bitwise_not(resized)
+                        gray_image1= cv2.cvtColor(invert,cv2.COLOR_BGR2GRAY)
+                        gray_image1=cv2.equalizeHist(gray_image1)
+                        threshold=cv2.threshold(gray_image1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+                        #rem_noise=cv2.medianBlur(threshold,5)
+                        #PyTesseract to convert text in the image to string
+                        plate = pytesseract.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
+                        #reader = easyocr.Reader(['en'],model_storage_directory='.')
+                        #plate = reader.readtext(threshold,paragraph="False")[0][1]
+                        if(plate==''):
+                            plate="Error"
+                        if(len(plate)>10): #Car number plate can not have more than 10 characters 
+                            st.subheader("Number plate is : "+plate[:13])  
+                        else:
+                            st.subheader("Number plate is : "+plate)  
+                        break
+                cv2.drawContours(image,[screenCnt],-1,(0,255,0),2) 
+
+
+
+    
     if images is not None:
         file_bytes = np.asarray(bytearray(images.read()), dtype=np.uint8)
         image=cv2.imdecode(file_bytes,1)
